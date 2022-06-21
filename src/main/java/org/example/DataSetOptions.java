4ee41cbe8619141a2cdc3a5ca52e4e4444634167
Aleksandr <56115945+AlSavIg@users.noreply.github.com+alesavigo@gmail.com>
2022-06-21 22:02:31 +0300

    Убраны бесполезные комментарии

--- src/main/java/org/example/DataSetOptions.java
+++ src/main/java/org/example/DataSetOptions.java
@@ -6,9 +6,6 @@ import java.util.Set;
 public interface DataSetOptions {
     Double getMiddleClientsAge();
     List<String> getTitlesOfTheMostPopularCourses(long limit);
-    // Get !three! of course titles which contains more people than other
     Set<String> getAllClientsInfo();
-    // Format: [client's] "name,age,courseTitle,courseDuration\n" [courseDuration] = month
     List<String> getAllClientsSortedByAge();
-    // The same format (decreasing)
 }
